f03940b75700cea0a37ab9a76dc87a5c5b3178df
andreeanafornita <nafornitadaniela21@stud.ase.ro>
2024-04-02 18:36:14 +0300

    changed footer sizes and added to database new entries

--- reactfoodapp/src/components/Footer.js
+++ reactfoodapp/src/components/Footer.js
@@ -3,17 +3,17 @@ export default function Footer(){
         <div className="footer container">
             <div className="footer-section">
                 <p className="title">About FridgeFest</p>
-                <p>FridgeFest is not a common fest! Here you can please your soul and tummy with delicious meals generated specially for your needs."</p>
+                <p>FridgeFest is not a common fest! Here you can please your soul and tummy with delicious meals generated specially for your needs.</p>
                 <p>&copy; 2024 | All rights reserved</p>
             </div>
             <div className="footer-section">
                 <p className="title">Contact us</p>
-                <p>fridgefest@gmail.com</p>
+                <p>fridgefestapp@gmail.com</p>
             </div>
             <div className="footer-section">
                 <p className="title">Socials</p>
-                <p>Facebook</p>
-                <p>Twitter</p>
+                <p><a href="https://www.facebook.com/profile.php?id=61557537310306" target="_blank" rel="noopener noreferrer">Facebook</a></p>
+                <p><a href="https://twitter.com/FridgeFest85669" target="_blank" rel="noopener noreferrer">Twitter</a></p>
                 <p>Instagram</p>
             </div>
         </div>
